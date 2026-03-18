fc382f624deb5fc7bab13c80393f54199e1e301d
Inndy <inndy.tw@gmail.com>
2026-03-18 11:39:51 +0800

    Make sub-command share as default behavior

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
 
 var rootCmd = &cobra.Command{
 	Use:   "liveshare",
@@ -14,5 +18,24 @@ func init() {
 }
 
 func Execute() error {
+	if args := os.Args[1:]; len(args) > 0 && args[0] != "" && args[0][0] != '-' {
+		firstArg := args[0]
+		known := false
+		for _, cmd := range rootCmd.Commands() {
+			if cmd.Name() == firstArg {
+				known = true
+				break
+			}
+			for _, alias := range cmd.Aliases {
+				if alias == firstArg {
+					known = true
+					break
+				}
+			}
+		}
+		if !known && firstArg != "help" && firstArg != "completion" {
+			rootCmd.SetArgs(append([]string{"share"}, args...))
+		}
+	}
 	return rootCmd.Execute()
 }
